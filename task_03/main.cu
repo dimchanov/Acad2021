115c5347483f3cc613b3ece01c1912d6712ba4b3
Dmitry Novikov <dimchanov1121@mail.ru>
2021-09-30 01:29:23 +0300

    task_03 div

--- task_03/main.cu
+++ task_03/main.cu
@@ -6,159 +6,83 @@
 #include <cmath>
 #include <limits>
 
-using namespace std;
 
-__global__ void sum_vectors_for(double *a, double *b, double *c, int size){
+__global__ void sum_vectors_if(double *a, int size){
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < size) {
-        for (int i = 0; i < 1000; ++i) {
-            double num;
-            if (idx + i < size) {
-                num = a[idx + i] + b[idx + i];
-            }
-            if (i == 0) {
-                c[idx] = num;
-            }
-        }
-    }
-}
-
-__global__ void sum_vectors_if(double *a, double *b, double *c, int size){
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < size) {
-        int i = idx % 5;
-        double num;
-        if (i) {
-            if (idx + i < n) {
-                num = a[idx + i % 5] + b[idx + i % 5];
-            }
+        if ((int(a[idx]) + idx) % 3 == 0) {
+            a[idx] = a[idx / 3] * (idx + 2);
         } else {
-            num = a[idx + i] + b[idx + i];
+            if ((int(a[idx]) + idx) % 3 == 1) {
+                a[idx] = a[idx / 3] * (idx + 1);
+            } else {
+                a[idx] = a[idx / 3] * (idx - 2);
+            }
         }
-        c[idx] = num;
     }
 }
 
-__global__ void sum_vectors(double *a, double *b, double *c, int size){
+__global__ void sum_vectors(double *a, int size){
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < size) {
-        c[idx] = a[idx] + b[idx];
+        a[idx] += idx % 3;
     }
-}
-
-int check(double *a, double *b, double *c, int size) {
-    for (int i = 0; i < size; ++i) {
-        if (std::fabs(c[i] - (a[i] + b[i])) > std::numeric_limits<double>::epsilon()) {
-            return 0;
-        }
-    }
-    return 1;
 }
 
 int main(int argc, char **argv){
     int n = (int)strtol(argv[1], NULL, 10);
 
-    double *h_a, *h_b, *h_c;
-
     size_t bytes = n * sizeof(double);
 
+    double *h_a;
     h_a = (double *) malloc(bytes);
-    h_b = (double *) malloc(bytes);
-    h_c = (double *) malloc(bytes);
 
+    srand(3333);
     for (int i = 0; i < n; i++){
-        h_a[i] = sin(i) * sin(i);
-        h_b[i] = cos(i) * cos(i);
+        int num = rand();
+        h_a[i] = num - num % 3;
     }
 
-    double *d_a, *d_b, *d_c;
-
+    double *d_a;
     cudaMalloc(&d_a, bytes);
-    cudaMalloc(&d_b, bytes);
-    cudaMalloc(&d_c, bytes);
+    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
 
     int blockSize, gridSize;
     blockSize = 1024;
     gridSize = (n - 1) / 1024 + 1;
 
     cudaEvent_t start, stop;
-
-    //  sum_vectors_for_time
-    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start);
-
-    sum_vectors_for<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
-    cudaDeviceSynchronize();
-    cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
-    float sum_vectors_for_time = 0;
-    cudaEventElapsedTime(&sum_vectors_for_time, start, stop);
-    cudaMemcpy(h_c, d_c, bytes, cudaMemcpyDeviceToHost);
-    
-    int check_flag = check(h_a, h_b, h_c, n);
-    if (check_flag) {
-        std::cout << "Correct sum sum_vectors_for" << std::endl;
-    } else {
-        std::cout << "Not correct sum sum_vectors_for" << std::endl;
-    }
-    std::cout << "Gpu time: " << sum_vectors_for_time << " sum_vectors_for_time" << std::endl;
-
     //  sum_vectors_if
-    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start);
 
-    sum_vectors_if<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
+    sum_vectors_if<<<gridSize, blockSize>>>(d_a, n);
     cudaDeviceSynchronize();
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     float sum_vectors_if = 0;
     cudaEventElapsedTime(&sum_vectors_if, start, stop);
-    cudaMemcpy(h_c, d_c, bytes, cudaMemcpyDeviceToHost);
-    
-    check_flag = check(h_a, h_b, h_c, n);
-    if (check_flag) {
-        std::cout << "Correct sum sum_vectors_if" << std::endl;
-    } else {
-        std::cout << "Not correct sum sum_vectors_if" << std::endl;
-    }
     std::cout << "Gpu time: " << sum_vectors_if << " sum_vectors_if" << std::endl;
 
     //  sum_vectors
-    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start);
 
-    sum_vectors<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
+    sum_vectors<<<gridSize, blockSize>>>(d_a, n);
     cudaDeviceSynchronize();
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     float sum_vectors = 0;
     cudaEventElapsedTime(&sum_vectors, start, stop);
-    cudaMemcpy(h_c, d_c, bytes, cudaMemcpyDeviceToHost);
     
-    check_flag = check(h_a, h_b, h_c, n);
-    if (check_flag) {
-        std::cout << "Correct sum sum_vectors" << std::endl;
-    } else {
-        std::cout << "Not correct sum sum_vectors" << std::endl;
-    }
     std::cout << "Gpu time: " << sum_vectors << " sum_vectors" << std::endl;
 
+
     cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
 
     free(h_a);
-    free(h_b);
-    free(h_c);
 
     return 0;
 }
